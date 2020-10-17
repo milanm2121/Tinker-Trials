6e4f4f5335009f43b36599da347ef3fc6a2697a0
milanm2121 <55521009+milanm2121@users.noreply.github.com>
2020-10-17 13:28:24 +0800

    tri2 v0.04
    added a basic lobby room joining system

---
+++ tinker trials/Assets/scripts/multiplayer/multiplayer_lobby_maneger.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using TMPro;
+
+public class multiplayer_lobby_maneger : MonoBehaviourPunCallbacks
+{
+    public TMP_Text[] playerIDs;
+
+
+    public override void OnJoinedRoom()
+    {
+        base.OnJoinedRoom();
+
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PhotonNetwork.CurrentRoom != null)
+        {
+
+
+            for (int i = 0; PhotonNetwork.CurrentRoom.PlayerCount > i; i++)
+            {
+
+                playerIDs[i].text = PhotonNetwork.PlayerList[i].NickName;
+                if (PhotonNetwork.PlayerList[i].IsMasterClient)
+                {
+                    playerIDs[i].color = Color.blue;
+                }
+            }
+            for (int i = PhotonNetwork.CurrentRoom.PlayerCount; playerIDs.Length > i; i++)
+            {
+                playerIDs[i].text = "finding players...";
+            }
+         
+        }
+    }
+    public override void OnLeftRoom()
+    {
+        base.OnLeftRoom();
+        if (PhotonNetwork.IsMasterClient && PhotonNetwork.CurrentRoom.PlayerCount>1)
+        {
+            PhotonNetwork.SetMasterClient(PhotonNetwork.CurrentRoom.Players[2]);
+        }
+    }
+
+}
---
+++ tinker trials/Assets/scripts/multiplayer/multiplayer_menue_maneger.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class multiplayer_menue_maneger : MonoBehaviourPunCallbacks
+{
+    public GameObject[] active_on_online_buttons;
+    public GameObject connect_button;
+    public multiplayer_launcher ML;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ML = GameObject.Find("multiplayer_launcher").GetComponent<multiplayer_launcher>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PhotonNetwork.IsConnectedAndReady == true)
+        {
+            for(int i=0;active_on_online_buttons.Length>i; i++)
+            {
+                active_on_online_buttons[i].SetActive(true);
+            }
+            connect_button.SetActive(false);
+        }
+        else
+        {
+            for (int i = 0; active_on_online_buttons.Length > i; i++)
+            {
+                active_on_online_buttons[i].SetActive(false);
+            }
+            connect_button.SetActive(true);
+        }
+    }
+
+    
+    
+}
