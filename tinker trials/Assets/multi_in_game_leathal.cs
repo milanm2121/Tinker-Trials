20684f6902375605e763917e2ed1a855721cd8d1
milanm2121 <55521009+milanm2121@users.noreply.github.com>
2020-11-05 11:09:41 +0800

    started multi player lethal bug fixing

---
+++ tinker trials/Assets/multi_in_game_leathal.cs
@@ -0,0 +1,167 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class multi_in_game_leathal : MonoBehaviour
+{
+    public GameObject primer;
+    public primer primer_script;
+
+    public GameObject Payload;
+    public payload payload_script;
+
+    public GameObject container;
+    public container container_script;
+
+    public bool manual = true;
+    public bool primed = false;
+
+    public PhotonView PV;
+
+    float fuze = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PV = GetComponent<PhotonView>();
+        PV.RPC("generate", RpcTarget.AllBuffered);
+        generate();
+        if (primer_script.PO.manual == false)
+        {
+            fuze = primer_script.PO.timer;
+            Debug.Log(fuze);
+            manual = false;
+        }
+        else
+        {
+            manual = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (manual == false)
+        {
+            fuze -= Time.deltaTime;
+        }
+        if(fuze>=0 && manual == false)
+        {
+            StartCoroutine(primerPhase());
+        }
+
+        if (fuze <= 0 && manual == false)
+        {
+            PV.RPC("explode", RpcTarget.All);
+            print("boom");
+        }
+        if (manual == true && primed == true)
+        {
+            PV.RPC("explode", RpcTarget.All);
+            print("boom1");
+        }
+        
+    }
+    [PunRPC]
+    void generate()
+    {
+        primer_script.GeneratePrimer();
+        payload_script.generatePayload();
+        container_script.generateContainer();
+    }
+    [PunRPC]
+    void explode()
+    {
+        Collider[] col = Physics.OverlapSphere(transform.position, payload_script.PO.radious * 2);
+        if (PhotonNetwork.IsMasterClient)
+        {
+            for (int i = 0; col.Length > i; i++)
+            {
+                RaycastHit hit;
+
+                Vector3 offset = (col[i].transform.position - transform.position);
+                Physics.Raycast(transform.position, offset, out hit);
+                if (hit.collider == col[i] && col[i].GetComponent<Rigidbody>() != null)
+                {
+                    col[i].attachedRigidbody.velocity += 10 * (new Vector3(1, 1, 1) - (offset / offset.magnitude));
+                    if (col[i].GetComponent<multi_player_stats>() != null)
+                    {
+                        col[i].GetComponent<multi_player_stats>().damage_player(payload_script.PO.damage * 100, new Vector2Int(payload_script.PO.element, 0));
+                    }
+                    if (col[i].gameObject.GetComponent<object_health>() != null)
+                    {
+                        col[i].gameObject.GetComponent<object_health>().damage_object(payload_script.PO.damage * 10);
+                    }
+                }
+            }
+        }
+
+        GameObject x = Instantiate(live_refrence_maneger.Expolsion, transform.position, Quaternion.identity);
+        x.GetComponent<explosion_groth>().raidious = payload_script.PO.radious * 2;
+
+        if (payload_script.PO.element == 0)
+        {
+            x.GetComponent<MeshRenderer>().material = live_refrence_maneger.EXPdefalt;
+        }
+        else if (payload_script.PO.element == 1)
+        {
+            x.GetComponent<MeshRenderer>().material = live_refrence_maneger.EXPFire;
+        }
+        else if (payload_script.PO.element == 2)
+        {
+            x.GetComponent<MeshRenderer>().material = live_refrence_maneger.EXPice;
+        }
+        else if (payload_script.PO.element == 3)
+        {
+            x.GetComponent<MeshRenderer>().material = live_refrence_maneger.EXPearth;
+        }
+        else if (payload_script.PO.element == 4)
+        {
+            x.GetComponent<MeshRenderer>().material = live_refrence_maneger.EXPEelctrisity;
+        }
+
+
+        Destroy(gameObject);
+    }
+
+    IEnumerator primerPhase()
+    {
+        if (primer_script.PO.speciality == 1)
+        {
+            PV.RPC("vortex", RpcTarget.All);
+            yield return new WaitForSeconds(2);
+            PV.RPC("vortex", RpcTarget.All);
+        }
+        PV.RPC("explode", RpcTarget.All);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (container_script.CO.sticky == true)
+        {
+            transform.parent = collision.transform;
+            GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+        }
+    }
+
+
+    void vortex()
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            List<Rigidbody> targets = new List<Rigidbody>();
+            Collider[] col = Physics.OverlapSphere(transform.position + (-transform.right.normalized * 1), 10);
+            for (int i = 0; col.Length > i; i++)
+            {
+                if (col[i].GetComponent<Rigidbody>() != null && col[i].gameObject.layer == 9)
+                    targets.Add(col[i].GetComponent<Rigidbody>());
+            }
+            for (int i = 0; targets.Count > i; i++)
+            {
+                Vector3 offest = (transform.position - targets[i].transform.position);
+                targets[i].velocity += offest.normalized * (10 * offest.magnitude);
+            }
+        }
+    }
+
+}
---
+++ tinker trials/Assets/multi_lethal_thrower.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class multi_lethal_thrower : MonoBehaviour
+{
+    public GameObject lethal;
+    multi_in_game_leathal IGL;
+
+    public GameObject currentLeathal;
+
+    public bool tapped_lethal;
+    public PhotonView PV;
+    public player_classes_loader PCL;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        PV = GetComponent<PhotonView>();
+    }
+
+
+    public void initalise()
+    {
+        PCL = GameObject.Find("multiplayer_game_maneger").GetComponent<player_classes_loader>();
+    }
+
+
+    public void loadClass(string UserID, int Class)
+    {
+        object[] x = { UserID, Class };
+        PV.RPC("syncClass", RpcTarget.AllBuffered, x);
+    }
+
+
+    [PunRPC]
+    void syncClass(string UserID, int Class)
+    {
+        if (Class == 1)
+        {
+            generate_leathal(PCL.playerClasses[UserID].class_1);
+        }
+        else if (Class == 2)
+        {
+            generate_leathal(PCL.playerClasses[UserID].class_2);
+        }
+        else if (Class == 3)
+        {
+            generate_leathal(PCL.playerClasses[UserID].class_3);
+        }
+        else if (Class == 4)
+        {
+            generate_leathal(PCL.playerClasses[UserID].class_4);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G) && currentLeathal == null)
+        {
+            GameObject x = PhotonNetwork.Instantiate(IGL.name, transform.position, Quaternion.identity);
+            IGL.primed = false;
+            x.GetComponent<Rigidbody>().velocity = transform.forward * IGL.container_script.CO.weight * 10;
+            currentLeathal = x;
+            tapped_lethal = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.G) && tapped_lethal == false)
+        {
+            if (currentLeathal != null && IGL.manual == true)
+            {
+                currentLeathal.GetComponent<multi_in_game_leathal>().primed = true;
+                currentLeathal = null;
+
+            }
+        }
+        tapped_lethal = false;
+
+    }
+
+    void generate_leathal(class_class.Class class_)
+    {
+        IGL = lethal.GetComponent<multi_in_game_leathal>();
+        IGL.primer_script.PO = class_.Lethal.primer;
+        IGL.container_script.CO = class_.Lethal.container;
+        IGL.payload_script.PO = class_.Lethal.payload;
+    }
+    
+}
