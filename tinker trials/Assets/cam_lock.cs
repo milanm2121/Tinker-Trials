582a9daa306f6c7a18359d1df2ace788c55be35d
milanm2121 <55521009+milanm2121@users.noreply.github.com>
2020-11-19 15:06:04 +0800

    added mini map

---
+++ tinker trials/Assets/cam_lock.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cam_lock : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, 60, transform.position.z);
+    }
+}
