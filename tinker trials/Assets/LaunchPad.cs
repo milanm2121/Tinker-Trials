d98916b9f73869ee461a1ec3b95e17ebbd50db32
Red <43746753+RedJerome@users.noreply.github.com>
2020-12-09 11:14:34 +0800

    canyon things
    done

---
+++ tinker trials/Assets/LaunchPad.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaunchPad : MonoBehaviour
+{
+    public float pushPower;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        collision.gameObject.GetComponent<Rigidbody>().AddForce(transform.up * pushPower);
+
+    }
+}
